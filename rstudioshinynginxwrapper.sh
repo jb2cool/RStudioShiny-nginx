8cb8a425d2bb893c462c79c9a858ffd7fec02656
Jason Bramwell <jb2cool@gmail.com>
2020-06-26 18:12:40 +0100

    Update rstudioshinynginxwrapper.sh

--- rstudioshinynginxwrapper.sh
+++ rstudioshinynginxwrapper.sh
@@ -41,7 +41,12 @@ mkdir $HOME/shiny
 # Copy sample apps to users new Shiny dir
 cp -r /opt/shiny-server/samples/sample-apps/hello/ ~/shiny
 
-# Tell user to reboot
+# Restart services
 sudo systemctl restart nginx
 sudo systemctl enable nginx
 sudo systemctl restart shiny-server
+
+#Tell user everything works
+echo nginx is now hosting a webpage on http://127.0.0.1
+echo RStudio Server is now available on http://127.0.0.1:8787 & http://127.0.0.1/rstudio
+echo Shiny Server is now available on http://127.0.0.1:3838 & http://127.0.0.1/shiny
