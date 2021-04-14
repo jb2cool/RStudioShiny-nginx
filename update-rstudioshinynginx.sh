0d8750c470773c96b4160d1795a2a905d34804d9
Jason Bramwell <jb2cool@gmail.com>
2021-04-14 13:03:23 +0100

    Update update-rstudioshinynginx.sh

--- update-rstudioshinynginx.sh
+++ update-rstudioshinynginx.sh
@@ -18,4 +18,4 @@ sudo apt-get install curl -y
 VERSION=$(curl https://download3.rstudio.org/ubuntu-14.04/x86_64/VERSION)
 wget --no-verbose "https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-$VERSION-amd64.deb" -O shiny-server-latest.deb
 sudo gdebi -n shiny-server-latest.deb
-rm -f version.txt shiny-server-latest.deb
+rm shiny-server-latest.deb
