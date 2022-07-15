82d2f6a8223b166ac9cb8ad1c673094f230240fa
Jason Bramwell <jb2cool@gmail.com>
2022-07-15 09:23:31 +0100

    Update install-rstudioshinynginx.sh

--- install-rstudioshinynginx.sh
+++ install-rstudioshinynginx.sh
@@ -38,6 +38,7 @@ rm shiny-server-latest.deb
 sudo sed -i "s/run_as shiny/run_as $USER/" /etc/shiny-server/shiny-server.conf
 sudo sed -i "s/3838/ 3838 0.0.0.0/" /etc/shiny-server/shiny-server.conf
 sudo sed -i "s/site_dir \/srv\/shiny-server/site_dir \/home\/$USER\/shiny/" /etc/shiny-server/shiny-server.conf
+sudo sed -i '/directory_index on;$/a \ \ \ \ sanitize_errors off;\n \ \ \ disable_protocols xdr-streaming xhr-streaming iframe-eventsource iframe-htmlfile;' /etc/shiny-server/shiny-server.conf
 mkdir $HOME/shiny
 
 # Copy sample apps to users new Shiny dir
