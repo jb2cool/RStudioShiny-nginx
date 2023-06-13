cae2ceee22278cc006914f00fa2596b36b045f27
Jason Bramwell <jb2cool@gmail.com>
2023-06-13 15:24:56 +0100

    Update update-rstudioshinynginx.sh

--- update-rstudioshinynginx.sh
+++ update-rstudioshinynginx.sh
@@ -6,7 +6,7 @@ sudo apt-get update && sudo apt-get install r-base r-base-dev -y
 
 # Install RStudio Server
 sudo apt-get install gdebi-core -y
-wget https://www.rstudio.org/download/latest/stable/server/bionic/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
+wget https://www.rstudio.org/download/latest/stable/server/focal/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
 sudo gdebi --non-interactive rstudio-latest.deb
 rm rstudio-latest.deb
 
