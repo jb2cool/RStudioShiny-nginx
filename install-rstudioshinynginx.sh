d63f22fa7ae9c21f2dca56d350b539e0880dfd18
Jason Bramwell <jb2cool@gmail.com>
2024-04-15 16:53:11 +0100

    Update install-rstudioshinynginx.sh

--- install-rstudioshinynginx.sh
+++ install-rstudioshinynginx.sh
@@ -33,7 +33,15 @@ sudo apt-get update && sudo apt-get install r-base r-base-dev -y
 
 # Install RStudio Server
 sudo apt-get install gdebi-core -y
-wget https://www.rstudio.org/download/latest/stable/server/focal/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
+if [[ $(lsb_release -rs) == "20.04" ]]
+then
+    wget https://www.rstudio.org/download/latest/stable/server/focal/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
+elif [[ $(lsb_release -rs) == "22.04" ]]
+then
+    wget https://www.rstudio.org/download/latest/stable/server/jammy/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
+else
+    echo "Non-compatible version"
+fi
 sudo gdebi --non-interactive rstudio-latest.deb
 rm rstudio-latest.deb
 
