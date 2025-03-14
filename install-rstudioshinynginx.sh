30065906b1124f63204fd795811387656d02feb4
Jason Bramwell <jb2cool@gmail.com>
2025-03-14 10:20:35 +0000

    Tidying up service restart commands

--- install-rstudioshinynginx.sh
+++ install-rstudioshinynginx.sh
@@ -67,10 +67,8 @@ mkdir $HOME/shiny
 cp -r /opt/shiny-server/samples/sample-apps/hello/ ~/shiny
 
 # Restart services
-sudo systemctl daemon-reload
-sudo systemctl restart nginx
-sudo systemctl enable nginx
-sudo systemctl restart shiny-server
+sudo systemctl reload nginx
+sudo systemctl reload shiny-server
 
 # Clean up install script
 rm install-rstudioshinynginx.sh
